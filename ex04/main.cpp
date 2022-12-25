af6c715a58bcdb14089b78e5a4f962afbd1fa7da
NajmiAchraf <najmi.achraf@gmail.com>
2022-12-26 00:47:43 +0100

    Changes to be committed: 	modified:   .gitignore 	modified:   ex00/Makefile 	modified:   ex01/Makefile 	modified:   ex01/Zombie.hpp 	modified:   ex02/Makefile 	modified:   ex03/Makefile 	new file:   ex04/Makefile 	new file:   ex04/main.cpp 	new file:   ex04/main0.cpp

---
+++ ex04/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+static std::string	replacing(std::string line, std::string str1, std::string str2) {
+	std::string::size_type	start = 0;
+	std::string::size_type	n;
+
+	while ((n = line.find(str1, start)) != std::string::npos) {
+		line.erase(n, str1.length());
+		line.insert(n, str2);
+		start = n + str2.length();
+	}
+	return (line);
+}
+
+static void	error(std::string msg, std::string filename = "") {
+	std::cerr << "ERROR : "<< msg << filename << std::endl;
+	std::exit(EXIT_FAILURE);
+}
+
+int	main (int ac , char *av[]) {
+	if (ac != 4 || av[2] == NULL || av[3] == NULL)
+		error("Wrong Arguments!");
+
+	std::string	filename	= av[1];
+	std::string str1		= av[2];
+	std::string str2		= av[3];
+
+	std::ifstream	infile(filename.data());
+	if (!infile.is_open())
+		error("Cannot Open File!", filename);
+
+	std::ofstream	outfile(filename.append(".replace").data());
+	if (!outfile.is_open()) {
+		infile.close();
+		error("Cannot Open File!", filename.append(".replace"));
+	}
+	while (true) {
+		std::string	line;
+		std::getline(infile, line);
+		outfile << replacing(line, str1, str2);
+		if (infile.eof())
+			break;
+		outfile << std::endl;
+	}
+	infile.close();
+	outfile.close();
+	return (EXIT_SUCCESS);
+}
---
+++ ex04/main0.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+
+static std::string	ft_replace(std::string str, std::string s1, std::string s2) {
+	std::string result;
+	std::string::size_type start = 0;
+	std::string::size_type n;
+
+	while ((n = str.find(s1, start)) != std::string::npos) {
+		str.erase(n, s1.length());
+		str.insert(n, s2);
+		start = n + s2.length();
+	}
+	return (str);
+}
+
+int	main(int ac, char *av[]) {
+	if (ac != 4 || av[2] == NULL || av[3] == NULL) {
+		std::cerr << "Error: Wrong Arguments!" << std::endl;
+		return (1);
+	}
+
+	std::string	filename	= av[1];
+	std::string	s1			= av[2];
+	std::string	s2			= av[3];
+
+	std::ifstream	ifile(filename.data());
+	if (!ifile.is_open()) {
+		std::cerr << "Error: Wrong Filename!" << std::endl;
+		return (1);
+	}
+	std::ofstream ofile(filename.append(".replace").data());
+	if (!ofile.is_open()) {
+		ifile.close();
+		std::cerr << "Error: File Open Error" << std::endl;
+		return (1);
+	}
+
+	while (true) {
+		std::string	line;
+		std::getline(ifile, line);
+
+		ofile << ft_replace(line, s1, s2);
+		if (ifile.eof())
+			break;
+		ofile << std::endl;
+	}
+	ifile.close();
+	ofile.close();
+	return (0);
+}
